89ef8f14fea0ae401702c049d3b5878a8d58a058
2bsj <h3tmdwns@gmail.com>
2022-01-29 20:38:34 +0900

    processing : 22년 1월 5주차 알고리즘 풀이 수식 최대화 left

--- src/20220129/s67257.java
+++ src/20220129/s67257.java
@@ -1,13 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class s67257 {
+
+    List<Integer> numList = new ArrayList<>();
+    List<String> expList = new ArrayList<>();
+    Set<String> expSet = new HashSet<>();
+
     /**
      * https://programmers.co.kr/learn/courses/30/lessons/67257?language=java
      */
     public static void main(String[] args) {
-
+        s67257 problem = new s67257();
+        String expression = "100-200*300-500+20";
+        System.out.println(problem.solution(expression));
     }
 
     public long solution(String expression) {
+        String[] nums = expression.split("[-*+]");
+        String[] exps = expression.split("[0-9]{1,}");
+        for(String num : nums) {
+            numList.add(Integer.parseInt(num));
+        }
+
+        for(int i=1; i<exps.length; i++) {
+            expList.add(exps[i]);
+            expSet.add(exps[i]);
+        }
+
+        String[] setArray = expSet.toArray(new String[]{});
+        permutation(setArray,0, setArray.length, setArray.length);
+
         long answer = 0;
         return answer;
+    }
+
+    public void permutation(String[] exps, int depth, int n, int r) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++){
+
+            }
+            return;
+        }
+
+        for (int i = depth; i < n; i++) {
+            swap(exps, depth, i);
+            permutation(exps, depth + 1, n, r);
+            swap(exps, depth, i);
+        }
+    }
+
+    public void swap(String[] exps, int depth, int i) {
+        String temp = exps[depth];
+        exps[depth] = exps[i];
+        exps[i] = temp;
     }
 }
--- src/20220129/s72412.java
+++ src/20220129/s72412.java
@@ -6,9 +6,16 @@ public class s72412 {
      * https://programmers.co.kr/learn/courses/30/lessons/72412?language=java
      */
     public static void main(String[] args) {
-
+        s72412 problem = new s72412();
+        String[] info = new String[]{"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
+        String[] query = new String[]{"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
+        System.out.println(problem.solution(info,query));
     }
 
+    /**
+     * String 비교 solution
+     * 최악의경우 5만 x 10만의 경우의수로 효율성 실패함
+     */
     public int[] solution(String[] info, String[] query) {
         ArrayList[] infoListArray = new ArrayList[info.length];
         ArrayList[] queryListArray = new ArrayList[query.length];
